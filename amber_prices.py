dbd3248d0f5d8559e2ebf5452aebc9637fc82c87
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-05-18 17:48:03 +1000

    modified main script

--- amber_prices.py
+++ amber_prices.py
@@ -1,8 +1,6 @@
-from utils.amber_utils import AmberSummary
+if __name__ == "__main__":
 
-amber_obj = AmberSummary()
-energy_dataframe = amber_obj.create_energy_dataframe()
-energy_dataframe =  amber_obj.basic_formatting(energy_dataframe=energy_dataframe)
-amber_obj.summarize_energy(summary_level='day', energy_dataframe=energy_dataframe)
-amber_obj.summarize_energy(summary_level='month', energy_dataframe=energy_dataframe)
-amber_obj.summarize_energy(summary_level='year', energy_dataframe=energy_dataframe)
+    from utils.amber_utils import AmberSummary
+
+    amber_obj = AmberSummary()
+    amber_obj.trigger_job()
