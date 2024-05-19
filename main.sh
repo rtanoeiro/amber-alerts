45b50c2f85a0b2ffa9fc9534dc6e9fab51a63727
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2024-05-19 17:05:20 +1000

    updated shell script

--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
 cd ~/python-projects/amber_alerts/
+echo "Running Amber Alerts Pipeline"
 pipenv shell
+echo "Updating Pipfile.lock"
 pipenv update --outdated
 python amber_prices.py
+echo "Pipeline execution complete."
