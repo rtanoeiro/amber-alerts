6339ab3176432d96ba8b3726343c63424150095c
rtanoeiro <ramontanoeiro@gmail.com>
2024-10-27 17:48:54 +1100

    updated main script

--- amber_prices.py
+++ amber_prices.py
@@ -2,4 +2,5 @@ if __name__ == "__main__":
     from utils.amber_utils import Amber
 
     amber_obj = Amber()
-    amber_obj.get_usage()
+    results = amber_obj.get_usage()
+    amber_obj.unwrap_usage(results)
