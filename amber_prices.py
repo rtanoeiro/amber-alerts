efa5363a267673210a16ea8eb6a95207718a3f17
rtanoeiro <ramontanoeiro@gmail.com>
2024-10-27 23:04:35 +1100

    small updated on main script

--- amber_prices.py
+++ amber_prices.py
@@ -1,6 +1,8 @@
 if __name__ == "__main__":
     from utils.amber_utils import Amber
+    from utils.postgres import insert_into_usage_table
 
     amber_obj = Amber()
     results = amber_obj.get_usage()
-    amber_obj.unwrap_usage(results)
+    energy_results = amber_obj.unwrap_usage(results)
+    insert_into_usage_table(energy_results)
